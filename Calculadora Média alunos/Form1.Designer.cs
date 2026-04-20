1ca00bea3d4a7130851e6f361b97a4ff82a9f250
Kldevzin <invesportff@gmail.com>
2026-04-20 21:38:57 +0100

    Primeiro commit

---
+++ Calculadora Média alunos/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Calculadora_Média_alunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Adicionar = new System.Windows.Forms.Button();
+            this.textBox_nota1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.list_Alunos = new System.Windows.Forms.ListBox();
+            this.textBox_Aluno = new System.Windows.Forms.TextBox();
+            this.nota_1 = new System.Windows.Forms.Label();
+            this.textBox_nota3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_nota2 = new System.Windows.Forms.TextBox();
+            this.button_Remover_aluno = new System.Windows.Forms.Button();
+            this.button_estatistica = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Adicionar
+            // 
+            this.button_Adicionar.Location = new System.Drawing.Point(555, 224);
+            this.button_Adicionar.Name = "button_Adicionar";
+            this.button_Adicionar.Size = new System.Drawing.Size(126, 125);
+            this.button_Adicionar.TabIndex = 0;
+            this.button_Adicionar.Text = "Adicionar Aluno";
+            this.button_Adicionar.UseVisualStyleBackColor = true;
+            this.button_Adicionar.Click += new System.EventHandler(this.button_Adicionar_Click);
+            // 
+            // textBox_nota1
+            // 
+            this.textBox_nota1.Location = new System.Drawing.Point(872, 85);
+            this.textBox_nota1.Multiline = true;
+            this.textBox_nota1.Name = "textBox_nota1";
+            this.textBox_nota1.Size = new System.Drawing.Size(172, 53);
+            this.textBox_nota1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(598, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome do aluno";
+            // 
+            // list_Alunos
+            // 
+            this.list_Alunos.FormattingEnabled = true;
+            this.list_Alunos.IntegralHeight = false;
+            this.list_Alunos.ItemHeight = 16;
+            this.list_Alunos.Location = new System.Drawing.Point(12, 44);
+            this.list_Alunos.Name = "list_Alunos";
+            this.list_Alunos.Size = new System.Drawing.Size(537, 436);
+            this.list_Alunos.TabIndex = 4;
+            // 
+            // textBox_Aluno
+            // 
+            this.textBox_Aluno.Location = new System.Drawing.Point(555, 85);
+            this.textBox_Aluno.Multiline = true;
+            this.textBox_Aluno.Name = "textBox_Aluno";
+            this.textBox_Aluno.Size = new System.Drawing.Size(231, 47);
+            this.textBox_Aluno.TabIndex = 5;
+            // 
+            // nota_1
+            // 
+            this.nota_1.AutoSize = true;
+            this.nota_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nota_1.Location = new System.Drawing.Point(920, 44);
+            this.nota_1.Name = "nota_1";
+            this.nota_1.Size = new System.Drawing.Size(69, 25);
+            this.nota_1.TabIndex = 6;
+            this.nota_1.Text = "Nota 1";
+            // 
+            // textBox_nota3
+            // 
+            this.textBox_nota3.Location = new System.Drawing.Point(1314, 85);
+            this.textBox_nota3.Multiline = true;
+            this.textBox_nota3.Name = "textBox_nota3";
+            this.textBox_nota3.Size = new System.Drawing.Size(137, 47);
+            this.textBox_nota3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1145, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nota 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1333, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nota 3";
+            // 
+            // textBox_nota2
+            // 
+            this.textBox_nota2.Location = new System.Drawing.Point(1109, 85);
+            this.textBox_nota2.Multiline = true;
+            this.textBox_nota2.Name = "textBox_nota2";
+            this.textBox_nota2.Size = new System.Drawing.Size(154, 47);
+            this.textBox_nota2.TabIndex = 10;
+            // 
+            // button_Remover_aluno
+            // 
+            this.button_Remover_aluno.Location = new System.Drawing.Point(716, 224);
+            this.button_Remover_aluno.Name = "button_Remover_aluno";
+            this.button_Remover_aluno.Size = new System.Drawing.Size(126, 125);
+            this.button_Remover_aluno.TabIndex = 11;
+            this.button_Remover_aluno.Text = "Remover Aluno";
+            this.button_Remover_aluno.UseVisualStyleBackColor = true;
+            this.button_Remover_aluno.Click += new System.EventHandler(this.button_Remover_aluno_Click);
+            // 
+            // button_estatistica
+            // 
+            this.button_estatistica.Location = new System.Drawing.Point(886, 224);
+            this.button_estatistica.Name = "button_estatistica";
+            this.button_estatistica.Size = new System.Drawing.Size(126, 125);
+            this.button_estatistica.TabIndex = 12;
+            this.button_estatistica.Text = "Estatísticas";
+            this.button_estatistica.UseVisualStyleBackColor = true;
+            this.button_estatistica.Click += new System.EventHandler(this.button_estatistica_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1488, 505);
+            this.Controls.Add(this.button_estatistica);
+            this.Controls.Add(this.button_Remover_aluno);
+            this.Controls.Add(this.textBox_nota2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_nota3);
+            this.Controls.Add(this.nota_1);
+            this.Controls.Add(this.textBox_Aluno);
+            this.Controls.Add(this.list_Alunos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_nota1);
+            this.Controls.Add(this.button_Adicionar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Adicionar;
+        private System.Windows.Forms.TextBox textBox_nota1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox list_Alunos;
+        private System.Windows.Forms.TextBox textBox_Aluno;
+        private System.Windows.Forms.Label nota_1;
+        private System.Windows.Forms.TextBox textBox_nota3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_nota2;
+        private System.Windows.Forms.Button button_Remover_aluno;
+        private System.Windows.Forms.Button button_estatistica;
+    }
+}
+
---
+++ Calculadora Média alunos/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Média_alunos
+{
+    public partial class Form1 : Form
+    {
+        public struct aluno
+        {
+            public string nome;
+            public int nota1;
+            public int nota2;
+            public int nota3;
+            public double media;
+            public string situacao;
+        }
+
+
+        public static aluno?[] turma = new aluno?[100];
+
+
+        public void preencherLista()
+        {
+            // Para limpar a lista box para por outro aluno
+            list_Alunos.Items.Clear();
+            for (int i = 0; i < turma.Length; i++)
+            {  
+                if (turma[i] != null)
+                {
+                    string nome_Aluno = turma[i]?.nome;
+                    int nota_1 = turma[i]?.nota1 ?? 0;
+                    int nota_2 = turma[i]?.nota2 ?? 0;
+                    int nota_3 = turma[i]?.nota3 ?? 0;
+                    double media = turma[i]?.media ?? 0;
+                    string situacao_ = turma[i]?.situacao;
+                    if (media >= 10)
+                    {
+                        situacao_ = "Aprovado";
+                    }
+                    else
+                    {
+                        situacao_ = "Reprovado";
+                    }
+                    // Aqui abaixo é para mostrar na lista BOX
+                    string aux = $"Aluno:{nome_Aluno} | nota:{nota_1} | nota:{nota_2} | nota :{nota_3} | Média:{media:F2} | {situacao_}";
+                    list_Alunos.Items.Add(aux);
+                    
+                }
+            }
+
+          
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Adicionar_Click(object sender, EventArgs e)
+        {
+            aluno NovoAluno = new aluno();
+            for(int i = 0; i < turma.Length; i++)
+            {
+                if (turma[i] == null)
+                {
+                    NovoAluno.nome = textBox_Aluno.Text;
+                    NovoAluno.nota1 = Convert.ToInt32(textBox_nota1.Text);
+                    NovoAluno.nota2 = Convert.ToInt32(textBox_nota2.Text);
+                    NovoAluno.nota3 = Convert.ToInt32(textBox_nota3.Text);
+                    // verifica se a nota está entre 0 e 20, somente é guardado os valores quando estiver entre os extremos
+                    if (NovoAluno.nota1 < 0 || NovoAluno.nota1 > 20 ||
+                        NovoAluno.nota2 < 0 || NovoAluno.nota2 > 20 ||
+                        NovoAluno.nota3 < 0 || NovoAluno.nota3 > 20)
+                    {
+                        MessageBox.Show("As notas devem ser entre 0 e 20!");
+                        return; // para a execução e não guarda o aluno
+                    }
+                    NovoAluno.media = (NovoAluno.nota1 + NovoAluno.nota2 + NovoAluno.nota3) / 3.0;
+                    turma[i] = NovoAluno;
+                    MessageBox.Show("Aluno guardado na posição " + i);
+                    preencherLista();
+                    break;
+                }
+            }
+            // para limpar a textbox
+            textBox_Aluno.Clear();
+            textBox_nota1.Clear();
+            textBox_nota2.Clear();
+            textBox_nota3.Clear();
+            // criar um aluno novo e as variaveis não guarda o valor que antes tinha e zera tudo
+            // novamente, fica com nenhum valor atribuido até o utilizador introduzir novamentex'   
+            NovoAluno = new aluno();
+
+        }
+
+        private void button_Remover_aluno_Click(object sender, EventArgs e)
+        {
+            // Para clicar no aluno que você quer remover
+            int index = list_Alunos.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Seleciona um aluno primeiro!");
+                return;
+            }
+            turma[index] = null;
+            list_Alunos.Items.RemoveAt(index);
+            MessageBox.Show("Aluno removido da posição:"+index.ToString());
+        }
+
+        private void button_estatistica_Click(object sender, EventArgs e)
+        {
+            double melhor_media = 0.0;
+            double pior_media = 20.0;
+            int totaldeAlunos = 0;
+            int aprovado = 0;
+            int reprovado = 0;
+            for(int i = 0; i < turma.Length; i++)
+            {
+                if (turma[i] != null)
+                {
+                    totaldeAlunos++;
+                    if (turma[i].Value.media > melhor_media)
+                    {
+                        melhor_media = turma[i].Value.media;
+                    }
+                    if (turma[i].Value.media < pior_media)
+                    {
+                        pior_media = turma[i].Value.media;
+                    }
+                    if (turma[i].Value.media >= 10)
+                    {
+                        aprovado++;
+                    }
+                    else
+                    {
+                        reprovado++;
+                    }
+                }
+            }
+            if(totaldeAlunos == 0)
+            {
+                MessageBox.Show("Não há alunos registrados");
+                return;
+            }
+            MessageBox.Show($"Melhor média: {melhor_media:F2}\nPior média: {pior_media:F2}\nAprovados: {aprovado}\nReprovados: {reprovado}");
+        }
+    }
+}
